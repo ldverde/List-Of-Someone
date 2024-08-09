233e16b05352d6c207dad9b2c4a55aa5482a2ef6
Lucas Daniel <131909547+ldverde@users.noreply.github.com>
2024-08-08 22:27:24 -0300

    new commit

---
+++ Lista.js
@@ -0,0 +1,87 @@
+var lista = [
+  {
+    linkId:"JHwKaiF6XYA",
+    key:21
+  },
+  {
+    linkId: "hpInLJXPTxA",
+    key: 1,
+  },
+  {
+    linkId: "bzAejwtv0BU",
+    key: 2,
+  },
+  {
+    linkId: "83BMiGieYfc",
+    key: 3,
+  },
+  {
+    linkId: "G0uuV_b-V6M",
+    key: 4,
+  },
+  {
+    linkId: "7vPnp-H_hVA",
+    key: 5,
+  },
+  {
+    linkId: "QBSuSPNOkaI",
+    key: 6,
+  },
+  {
+    linkId: "zksNSfz091w",
+    key: 7,
+  },
+  {
+    linkId: "yhZY7tC11OQ",
+    key: 8,
+  },
+  {
+    linkId: "xbz2vQTOIO8",
+    key: 9,
+  },
+  {
+    linkId: "H9VNLDUIfgo",
+    key: 10,
+  },
+  {
+    linkId: "00cyjiL-2dc",
+    key: 11,
+  },
+  {
+    linkId: "3zmZHblXCoQ",
+    key: 12,
+  },
+  {
+    linkId: "4zY6XZtiVGY",
+    key: 13,
+  },
+  {
+    linkId: "9FtrJMm238c",
+    key: 14,
+  },
+  {
+    linkId: "w8uXeWg-NTI",
+    key: 15,
+  },
+  {
+    linkId: "13_QUWSL2iE",
+    key: 16,
+  },
+  {
+    linkId: "3OnjOv_TtXo",
+    key: 17,
+  },
+  {
+    linkId: "FM52kbLe6zg",
+    key: 18,
+  },
+  {
+    linkId: "FDOLsk6raWw",
+    key: 19,
+  },
+  {
+    linkId: "rt3qNY-bxSE",
+    key: 20,
+  },
+];
+export { lista };
---
+++ app/styles.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#282A36",
+    flex:1
+  },
+  Typi: {
+    height:620,
+    paddingLeft:20,
+    paddingRight:10,
+    marginTop: 30,
+  },
+  ver: {
+    alignItems: "center",
+    marginTop: 150,
+  },
+  bot: {
+    padding: 12,
+    borderRadius: 20,
+    backgroundColor: "#bc70fa",
+  },
+  tibut: {
+    fontSize: 18,
+    fontFamily: "ArefRuqaa_700Bold",
+  },
+  titulo: {
+    fontFamily: "Sunshiney_400Regular",
+    fontSize: 40,
+    color: "white",
+    marginLeft:7
+  },
+  Vimg: {
+    width: 380,
+    height: 210,
+    marginBottom: 30,
+  },
+  img: {
+    width: 380,
+    height: 210,
+  },
+  flat: {
+    marginTop: 20,
+    
+  },
+  rodaPe: {
+   height:120,
+    justifyContent:'center',
+    alignItems: "center",
+    backgroundColor: "#282A36",
+    marginTop: 35,
+  },
+  rodaText: {
+    fontSize: 23,
+    color: "white",
+    fontFamily: "JosefinSans_400Regular",
+  },
+  scroll:{
+    backgroundColor:'#282A36'
+  }
+});
+
+export default styles;
