f2b09f7aa466e05df55ce397e60beeec60594230
Lucas Daniel <131909547+ldverde@users.noreply.github.com>
2024-08-17 00:19:46 -0300

    good Update

--- app/Components/HomeScreen.js
+++ app/Components/HomeScreen.js
@@ -1,14 +1,22 @@
 import TypingText from './TypingText';
 import RodaPe from './RodaPe';
+import React, {useCallback} from 'react';
 import { FlatList, View } from 'react-native';
-import { lista } from '../../Lista';
-import renderItem from './Elements.flatlist/renderItem';
+import { lista } from '../../Lista'; 
 import styles from '../styles';
 import keyExtractor from './Elements.flatlist/keyExtractor';
 import getItemLayout from './Elements.flatlist/getItemLayout';
 import renderItemSeparator from './Elements.flatlist/renderItemSeparator';
+import Item from './Items'
 
 export default function HomeScreen() {
+  
+  const renderItem = useCallback(({item}) => (
+    <View key={item.key}>
+      <Item linkId={item.linkId} /> 
+    </View>
+   ), [])
+
     return (
       <View> 
         <FlatList
--- app/Components/TypingText.js
+++ app/Components/TypingText.js
@@ -1,13 +1,31 @@
-import React, {Component} from "react";
-import TypeWriter from 'react-native-typewriter';
+ 
 import { View } from 'react-native';
+import React, { Component } from 'react';
+import TypeWriter from 'react-native-typewriter';
 import styles from '../styles';
 
-export default class TypingText extends Component {
-    render() {
-      return <View>
-        <TypeWriter style={styles.titulo} initialDelay={1000} typing={1} minDelay={20} maxDelay={60}> Seja Bem Vinda a sua lista de vídeos, os quais foram recomendados por... você deve saber quem.</TypeWriter>
-        <TypeWriter style={styles.titulo} initialDelay={10000} typing={1} minDelay={20} maxDelay={60}> {"\n"}{"\n"} Bem, os vídeos estão logo abaixo, basta clicar:</TypeWriter>
+class TypingText extends Component {
+
+
+  render() {
+     let delayMap = [
+      { at: /!/, delay: 1000 }
+    ]
+    return (
+      <View>
+        <TypeWriter
+          style={styles.titulo}
+          initialDelay={2000}
+          typing={1}
+          minDelay={20}
+          maxDelay={60}
+          delayMap={delayMap} 
+        >
+          Seja Bem Vinda a sua lista de vídeos, os quais foram recomendados por... você deve saber quem! {"\n"}{"\n"}Bem, os vídeos estão logo abaixo, basta clicar:
+        </TypeWriter>
       </View>
-    }
+    );
   }
+}
+
+export default TypingText;
