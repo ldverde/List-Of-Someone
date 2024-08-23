bdcb6c805f208a1b32510de39b57230846258b34
Lucas Daniel <131909547+ldverde@users.noreply.github.com>
2024-08-22 23:57:49 -0300

    ..

--- app/Components/TypingText.js
+++ app/Components/TypingText.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View,Text } from 'react-native';
+import { View } from 'react-native';
 import TypeWriter from 'react-native-typewriter';
 import styles from '../styles';
 
